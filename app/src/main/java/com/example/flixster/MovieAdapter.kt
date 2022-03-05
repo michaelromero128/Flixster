60818221d780a65583d2795b6b3a0ff523d63392
Michael Romero <michaelromero128@gmail.com>
2022-03-04 23:15:08 -0500

    added unsafe key

--- app/src/main/java/com/example/flixster/MovieAdapter.kt
+++ app/src/main/java/com/example/flixster/MovieAdapter.kt
@@ -1,18 +1,20 @@
 package com.example.flixster
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 
-
+const val MOVIE_EXTRA = "MOVIE_EXTRA"
 class MovieAdapter(private val context: Context, private val movies: MutableList<Movie>)
     : RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener{
         private val tvTitle = itemView.findViewById<TextView>(R.id.tvTitle)
         private val ivPoster = itemView.findViewById<ImageView>(R.id.imageView)
         private val tvOverview = itemView.findViewById<TextView>(R.id.tvOverview)
@@ -21,6 +23,18 @@ class MovieAdapter(private val context: Context, private val movies: MutableList
             tvOverview.text = movie.overview
             Glide.with(context).load(movie.posterImageUrl).into(ivPoster)
 
+        }
+        init {
+            itemView.setOnClickListener(this)
+        }
+
+        override fun onClick(p0: View?) {
+            val movie =movies[adapterPosition]
+            //Toast.makeText(context,movie.title, Toast.LENGTH_SHORT).show()
+            val intent = Intent(context,DetailActivity::class.java)
+            intent.putExtra("movie_title",movie.title)
+            intent.putExtra(MOVIE_EXTRA,movie)
+            context.startActivity(intent)
         }
     }
 
